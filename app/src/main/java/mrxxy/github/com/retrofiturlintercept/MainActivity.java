e7faabeab445e35af130cfd8bf59f338d41158af
xiaoyichao <xiaoyichao@goldmantis.com>
2018-11-28 09:59:13 +0800

    update demo

--- app/src/main/java/mrxxy/github/com/retrofiturlintercept/MainActivity.java
+++ app/src/main/java/mrxxy/github/com/retrofiturlintercept/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import mrxxy.github.intercept.DomainManager;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -14,23 +15,23 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private ApiService apiService;
+    private TextView tvResult;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvResult = findViewById(R.id.tv_result);
         Retrofit retrofit = AppApplication.application.getRetrofit();
         apiService = retrofit.create(ApiService.class);
         DomainManager.getInstance().putDomain("test", "https://api.douban.com");
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    public void data(View view) {
         apiService.getData().enqueue(new Callback<GankBean>() {
             @Override
             public void onResponse(Call<GankBean> call, Response<GankBean> response) {
-                Log.d(TAG, "onResponse gank io: success");
+                tvResult.setText("gank io success");
             }
 
             @Override
@@ -44,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         apiService.getTestData(1220562).enqueue(new Callback<DoubanBean>() {
             @Override
             public void onResponse(Call<DoubanBean> call, Response<DoubanBean> response) {
-                Log.d(TAG, "onResponse douban : success");
+                tvResult.setText("douban success");
             }
 
             @Override
